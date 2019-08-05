dffa213939d5e19ec784d0ef259d2c65975798f4
sijeune <hermionemy@naver.com>
2019-08-05 14:34:07 +0900

    controller and actions

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/b/80ab952233b700191d06802254498ef5
@@ -0,0 +1,14 @@
+package lunchbox.model.member;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	DataSource ds;
+	Connection con;
+	PreparedStatement pstmt;
+	ResultSet rs;
+}
--- lunchbox/src/lunchbox/model/member/MemberDAO.java
+++ lunchbox/src/lunchbox/model/member/MemberDAO.java
@@ -3,6 +3,7 @@ package lunchbox.model.member;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
@@ -11,4 +12,32 @@ public class MemberDAO {
 	Connection con;
 	PreparedStatement pstmt;
 	ResultSet rs;
+	
+	public int isMember(MemberVO member){ 
+        String sql ="select member_pwd from boardmember where member_id=?"; 
+        int result=-1; 
+         
+        try{ 
+            pstmt=con.prepareStatement(sql); 
+            pstmt.setString(1, member.getMEMBER_ID()); 
+            rs = pstmt.executeQuery(); 
+             
+            if(rs.next()){ 
+                if(rs.getString("MEMBER_PW").equals( 
+                                    member.getMEMBER_PWD())){ 
+                    result=1;//일치. 
+                }else{ 
+                    result=0;//불일치. 
+                } 
+            }else{ 
+                result=-1;//아이디 존재하지 않음. 
+            } 
+        }catch(Exception ex){ 
+            System.out.println("isMember 에러: " + ex);             
+        }finally{ 
+            if(rs!=null) try{rs.close();}catch(SQLException ex){} 
+            if(pstmt!=null) try{pstmt.close();}catch(SQLException ex){} 
+        } 
+        return result; 
+    }
 }
